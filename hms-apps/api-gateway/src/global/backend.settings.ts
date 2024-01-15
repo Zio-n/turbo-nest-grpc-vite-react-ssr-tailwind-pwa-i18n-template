93b6b12fb09ee0b45c71326640929aed6068606e
Zio-n <zionchrist8@gmail.com>
2024-01-15 11:40:39 +0100

    Question 2 update - error still occuring

--- hms-apps/api-gateway/src/global/backend.settings.ts
+++ hms-apps/api-gateway/src/global/backend.settings.ts
@@ -4,13 +4,13 @@ export const initialContentMap = {
 }
 
 export const assetMap = {
-  'styles.css': '/assets/index-BStWD-cp.css',
-  'main.js': '/assets/index--J4OESUU.js',
+  'styles.css': '/assets/index-3j7BsUbk.css',
+  'main.js': '/assets/index-8nIsldGK.js',
   'manifest': '/manifest.webmanifest',
   'vite-plugin-pwa:register-sw': '/registerSW.js',
   'additional-styles': [],//sufficient to drop flowbite.min.css in src/assets folder, to be incorporated at build time
   //'additional-jss': ["https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.js", "/custom-assets/flowbite/dist/datepicker.min.js"],//manually copied to public asset folder from node-modules
-  'additional-jss': ["/cassets/flowbite/dist/flowbite.min.js", "/cassets/flowbite/dist/datepicker.min.js"],//manually copied to public asset folder from node-modules
+  'additional-jss': [],//manually copied to public asset folder from node-modules
   initialContentMap,
   baseUrl: '/',
   initialI18nStore: {},//to be used later with middleware
